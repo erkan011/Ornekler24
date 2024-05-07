ee50bd3ecf9c9a7d4fc7ef19eb820cf79e58be4d
Erkan Çalışkan <146715229+erkan011@users.noreply.github.com>
2024-05-07 14:01:30 +0300

    IndexedValue Kotlin'de bir sınıftır ve genellikle withIndex() işleviyle birlikte kullanılır. Bu sınıf, koleksiyon elemanlarının hem değerlerini hem de indislerini birlikte taşır. Bu şekilde, döngülerde veya işlevlerde koleksiyon elemanlarına ve indislerine erişmek için kullanılır.Döngü İçinde IndexedValue Kullanımı, Filtreleme İşlemleriyle IndexedValue Kullanımı , Indis Kullanarak Toplama İşlemi.

---
+++ src/main/kotlin/IndexedValue.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val numbers = listOf(0,1,2,3,4,5,6,7,8,9)
+    var sum = 0
+    for (indexValue in numbers.withIndex()){
+        sum += indexValue.index * indexValue.value
+    }
+    println("Sum: $sum")
+
+
+    val filteredValues = numbers.withIndex()
+        .filter { indexedValue -> indexedValue.index % 2 == 0 }
+        .map { indexedValue -> indexedValue.value * 2 }
+    println("FilterValues: $filteredValues")
+}
